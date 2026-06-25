aba21dc7b167ba3942b22b59b3d70a31ae1a7eb4
Anushka Dubey <136051483+anushkaa-dubey@users.noreply.github.com>
2026-06-25 23:00:42 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer, Boolean> map=new HashMap<>();
+        for(int num: nums){
+            if(map.containsKey(num)){
+                return true;
+            }
+            map.put(num,true);
+        }
+      return false;
+    }
+}
